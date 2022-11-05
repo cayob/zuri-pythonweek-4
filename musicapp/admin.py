2c1cbe2ca7ca27f9ca368dc3803d746652d5c554
coyab <courageharvoxx@gmail.com>
2022-11-05 03:28:04 +0100

    Update to the assignment

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lyric, Song, Artiste
 
 # Register your models here.
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField( max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    date_of_release = models.DateField(default=datetime.today)
+    likes = models.CharField
+    artiste_id = models.CharField
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.TextField()
+    song_id = models.CharField
